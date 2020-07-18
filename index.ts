601a2147d92be65fb22aaaf1e42991b9e6993f84
Christian24 <christian.siebmanns@hotmail.de>
2020-07-18 14:20:10 +0200

    initial commit

---
+++ index.ts
@@ -0,0 +1,70 @@
+import { PreProcessHandler, Plugin, PluginContext, Schema } from 'dtsgenerator';
+import ts from 'typescript';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require('./package.json');
+
+/**
+ * This file is the main implementation for this plugin.
+ * Please edit this implementation.
+ */
+const plugin: Plugin = {
+    meta: {
+        name: packageJson.name,
+        version: packageJson.version,
+        description: packageJson.description,
+    },
+    // Remove the `preProcess` or `postProcess` if that is not needed for this plugin.
+    preProcess,
+    postProcess,
+};
+
+/**
+ * This `preProcess` is the hook for the input schema changing.
+ */
+async function preProcess(
+    pluginContext: PluginContext
+): Promise<PreProcessHandler | undefined> {
+    return (contents: Schema[]): Schema[] => {
+        console.log(
+            `PreProcess: config=<${JSON.stringify(pluginContext.option)}>`
+        );
+        return contents;
+    };
+}
+
+ async function traverseNodes(node: ts.Node, sourceFile: ts.SourceFile, printer: ts.Printer) {
+    console.log(`Kind ${node.kind}`);
+    
+    if (!ts.isModuleDeclaration(node) && !ts.isNamespaceExportDeclaration(node)) {
+    console.log(printer.printNode(ts.EmitHint.Unspecified, node,sourceFile)) + "\n";
+    }
+    
+    
+    node.forEachChild(child => traverseNodes(child, sourceFile, printer));
+
+} 
+
+/**
+ * This `postProcess` is the hook for the output AST changing.
+ */
+async function postProcess(
+    pluginContext: PluginContext
+): Promise<ts.TransformerFactory<ts.SourceFile> | undefined> {
+    return (_: ts.TransformationContext) => (
+        root: ts.SourceFile
+    ): ts.SourceFile => {
+        const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+      
+         traverseNodes(root, root, printer);
+        
+        
+      
+        console.log(
+            `PostProcess: config=<${JSON.stringify(pluginContext.option)}>`
+        );
+        return root;
+    };
+}
+
+export default plugin;
